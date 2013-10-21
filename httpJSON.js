40608c218a7a137671d85fda7da27baf39254637
ilteris <ilteris@gmail.com>
2013-10-20 22:16:25 -0400

    routes

--- httpJSON.js
+++ httpJSON.js
@@ -2,6 +2,19 @@ var http = require ('http');
 var url = require('url');
 
 
+var routes = {
+    "api/parsetime": function(parsedUrl) {
+        d = new Date(parsedUrl.query.iso);
+        return {
+            hour: d.getHours();
+            minute: d.getMinutes();
+            second: d.getSeconds();
+        };
+    },
+    "api/unixtime": function(parsedUrl) {
+        return {unixtime: (new Date(parsedUrl.query.iso)).getTime()};
+    }
+}
 
 server = http.createServer(function(reqest, response) {
     parsedUrl = url.parse(request.url, true);
