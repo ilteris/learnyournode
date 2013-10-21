6511130f5627d7fc442ba9a0e8c103599386a3d8
ilteris <ilteris@gmail.com>
2013-10-20 22:12:40 -0400

    http server

--- httpJSON.js
+++ httpJSON.js
@@ -6,5 +6,15 @@ var url = require('url');
 server = http.createServer(function(reqest, response) {
     parsedUrl = url.parse(request.url, true);
     resource = routes[parsedUrl.pathname];
-    
-}
+    if(resource) {
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify(resource(parsedUrl)));
+
+
+    } else {
+        response.writeHead(404);
+        response.end();
+    }
+});
+server.listen(8000);
+
