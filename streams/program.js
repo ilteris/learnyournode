27b7d557ccadaabb2b9987898a6c5006017b99fb
ilteris <ilteris@gmail.com>
2013-10-25 18:34:58 -0400

    solution to lines

--- streams/program.js
+++ streams/program.js
@@ -1,19 +1,26 @@
 var through = require('through');
 var split = require('split');
 
-var line = 0;
-var tr = through(function write(buf) {
-    line++;
-    console.log("line is " + line + "\n");
-    if (isOdd(line)) {
-        this.queue(buf.toString().toLowerCase());
-    }
-    else {
-        this.queue(buf.toString().toUpperCase());
-    }
-}, function end() {
-    this.queue(null); //don't expect any more data
-});
 
-function isOdd(num) { return num % 2; }
-process.stdin.pipe(tr).pipe(process.stdout);
+
+function count() {
+    var lines = 0;
+    return through(
+        function write (buf) {
+            lines++;
+            if(lines % 2) {
+                this.queue(buf.toString().toLowerCase() + '\n');
+            }
+            else
+            {
+                this.queue(buf.toString().toUpperCase() + '\n');
+            }
+        }, function end() {
+            this.emit(null); //don't expect any more data
+        });
+}
+
+process.stdin
+    .pipe(split())
+    .pipe(count())
+    .pipe(process.stdout);
