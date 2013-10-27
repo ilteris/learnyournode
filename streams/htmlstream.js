291ed55cb0449bd2ba2062e3cc7207478fb5e41d
ilteris <ilteris@gmail.com>
2013-10-27 19:15:13 -0400

    print out from through but trumpet is still problematic

--- streams/htmlstream.js
+++ streams/htmlstream.js
@@ -6,20 +6,20 @@ function changeToUpperCase() {
 	var tr = trumpet();
 	return through(
 		function write( buf ) {
-		this.queue(buf.toString()+ '\n');
-
+		this.emit('data', buf);
+		//this.emit(ws.toString().toUpperCase());
+		tr.pipe(this);
+		var ws = tr.select('.loud').createWriteStream();
+		ws.end('test');
+		console.log(ws);
 
 	},
 	function end() {
-		this.pipe(tr);
-		var ws = tr.select('.loud').createWriteStream();
-		this.queue(ws.toString().toUpperCase());
 		this.emit(null);
 	}
 	)
 }
 
-
 process.stdin
 .pipe(changeToUpperCase())
-.pipe(process.stdout);
+//.pipe(process.stdout);
