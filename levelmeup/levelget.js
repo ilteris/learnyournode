9fd123de1a1a8e37fe4c01907135ed05d92119c4
ilteris <ilteris@gmail.com>
2013-11-07 20:49:46 -0500

    console logs but unordered

--- levelmeup/levelget.js
+++ levelmeup/levelget.js
@@ -2,16 +2,12 @@ var level = require('level');
 var db = level(process.argv[2]);
 
 for (var i = 0; i < 100; i++) {
-	console.log(i);
+//	console.log(i);
 	var gibberish = "gibberish"+i;
-	console.log(gibberish);
-	db.get('gibberish', function(err, value) {
-		//if(err.type === 'NotFoundError') {
-
-	//	} else {
-	//		console.log("gibberish" + i + '=' + value);
-
-	//	}
-	)
-}
+	db.get(gibberish, function(err, value) {
+		if(!err)
+			{
+				console.log("gibberish" + '=' + value);
+			}
+	})
 }
