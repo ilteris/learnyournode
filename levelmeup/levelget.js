bfb53f547a2e01bafd27dd1921c1ee061e98ad53
ilteris <ilteris@gmail.com>
2013-11-08 20:21:13 -0500

    todo on the remaining get

--- levelmeup/levelget.js
+++ levelmeup/levelget.js
@@ -2,12 +2,13 @@ var level = require('level');
 var db = level(process.argv[2]);
 
 for (var i = 0; i < 100; i++) {
-//	console.log(i);
 	var gibberish = "gibberish"+i;
 	db.get(gibberish, function(err, value) {
 		if(!err)
 			{
 				console.log("gibberish" + '=' + value);
+				//got to save the results here
+				//then re order those and print them out.
 			}
 	})
 }
