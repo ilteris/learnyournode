a3d29170f37334144a00dc391481ca6127a95a2a
ilteris <ilteris@gmail.com>
2013-11-11 23:45:36 -0500

    with recursive

--- levelmeup/levelget.js
+++ levelmeup/levelget.js
@@ -1,7 +1,7 @@
 var level = require('level');
 var db = level(process.argv[2]);
 var Q = require('q');
-
+/*
 for (var i = 0; i < 100; i++) {
 	var gibberish = "gibberish"+i;
 	db.get(gibberish, function(err, value) {
@@ -13,4 +13,17 @@ for (var i = 0; i < 100; i++) {
 			}
 	})
 }
+*/
+recurse(0);
 
+function recurse (count) {
+    if (count <= 100) {
+        db.get('gibberish' + count, function (err, val) {
+                  if (val) console.log('gibberish' + count + '=' + val);
+                        recurse(count += 1);
+                            
+        });
+          
+    }
+
+}
