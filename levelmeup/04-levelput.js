c6de64f127a2c4a8e8e95a56a82fd8c9b4048d91
ilteris <ilteris@gmail.com>
2013-11-13 16:13:29 -0500

    leveldb put continue

--- levelmeup/04-levelput.js
+++ levelmeup/04-levelput.js
@@ -1,2 +1,8 @@
 var level = require('level');
 var db = level(process.argv[2]);
+
+db.put('foo', 'bar', function(err) {
+    if(err)
+        return console.error('Error in put():', err);
+    console.error('put foo = bar');
+})
